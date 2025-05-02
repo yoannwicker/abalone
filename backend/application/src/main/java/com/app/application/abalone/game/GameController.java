6759970c3fba84c9afe4f22a08b5d8b1eb305284
ywicker <yoann.wicker+pro@gmail.com>
2025-05-02 18:02:57 +0200

    TODO

--- backend/application/src/main/java/com/app/application/abalone/game/GameController.java
+++ backend/application/src/main/java/com/app/application/abalone/game/GameController.java
@@ -25,6 +25,18 @@ public class GameController {
     return ResponseEntity.ok(GameDto.fromGame(game));
   }
 
+  // TODO: Design responsive - trouver une solution pour deplacer les pions : rendre selectible les
+  // cases au extermités des pions selectionnés
+  // exemple : groupe selectionné = G4 - G6 ; cases selectionnables -> F3, G3, H4 du coté G4 et H7,
+  // G7, F6 du coté G6
+  // TODO: Design responsive - trouver une solution pour deplacer les pions : afficher les
+  // directions sur les cases pour les pions exterieurs sauf les pions aliés -> plus besoin des
+  // fleches
+  // TODO: Utiliser Redux pour gerer l'état du jeu
+  // TODO: se connecter pour jouer / Ajouter une partie de jeu (avec un id de partie)
+  // TODO: Ajouter la possibilité de faire null par accord (popup à modifier)
+  // TODO: Ajouter un timer / regle de null ou defaite ...etc -> url différente (choix du timer ?)
+  // TODO: Refactoring Usecase - regrouper OpponentPawnsToPush et PawnsToMove dans un meme objet ?
   @PostMapping(value = "move", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<PlayResultDto> move(@RequestBody MoveDto move) {
     try {
